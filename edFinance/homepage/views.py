34869d3843b18266d5f7e71760bf77baf134ae5f
shalomarputhasingh <shalomdevofficial@gmail.com>
2024-08-08 21:40:25 +0530

    update: Changed the Login Interface

--- edFinance/homepage/views.py
+++ edFinance/homepage/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def homepage(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard') 
     return render(request, 'homepage/home.html')
